797f33f9406c91eadd0c9d8b517c5dc6ffbe931d
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-09-23 12:25:48 +0200

    register post action method

--- Appointment/Controllers/AccountController.cs
+++ Appointment/Controllers/AccountController.cs
@@ -1,16 +1,24 @@
 ﻿using Appointment.Models;
+using Appointment.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Appointment.Controllers
 {
     public class AccountController : Controller
     {
-
         private readonly ApplicationDbContext _db;
+        UserManager<ApplicationUser> _userManager;
+        SignInManager<ApplicationUser> _signInManager;
+        RoleManager<IdentityRole> _roleManager;
 
-        public AccountController(ApplicationDbContext db)
+        public AccountController(ApplicationDbContext db, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager)
         {
             _db = db;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+            _userManager = userManager;
         }
         public IActionResult Login()
         {
@@ -19,6 +27,36 @@ namespace Appointment.Controllers
 
         public IActionResult Register()
         {
+            if (!_roleManager.RoleExistsAsync(Helper.Helper.Admin).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(Helper.Helper.Admin));
+                _roleManager.CreateAsync(new IdentityRole(Helper.Helper.Doctor));
+                _roleManager.CreateAsync(new IdentityRole(Helper.Helper.Patient));
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    Name = model.Name
+                };
+
+                var result = await _userManager.CreateAsync(user);
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, model.RoleName);
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
             return View();
         }
     }
